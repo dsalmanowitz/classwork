ed2fab8aa6b1dbd86d80375f4c3b6bd31ab253f0
Dennis Salmanowitz <dsalmanowitz@ucvts.org>
2018-11-19 11:05:07 -0500

    a

---
+++ classwork/src/nameScan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class nameScan {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = in.nextLine();
+		System.out.println(name);
+		in.close();
+	}
+}
